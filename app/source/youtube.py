a444ca99393c2b3bf98f2d055c8ef9b97c054146
Oscar Renalias <oscar@renalias.net>
2010-02-15 01:13:24 +0200

    Added basic support for youtube Added support for browsing entries of specific type via /entry/<source> Adapted the settings screen to use the forms framework

---
+++ app/source/youtube.py
@@ -0,0 +1,62 @@
+from app.source import Source
+from app.google import gdata
+from app.google.gdata import youtube
+from app.google.gdata.youtube import service
+from app.google.gdata.alt import appengine
+from app.models.config import Config
+from app.models.entry import Entry
+from app.dateutil.parser import *
+import logging
+
+class YouTubeSource(Source):
+	
+	def YouTubeSource(self):
+		self.source_id = 'youtube'
+		
+	def getFeedUri(self, **params):
+		uri = 'http://gdata.youtube.com/feeds/api/users/' + params['user']
+		if params['feed'] != None:
+			uri = uri + '/' + params['feed']
+		if params['count'] != None:
+			uri = uri + '?max-results=' + params['count']
+			
+		return uri
+	
+	def getFlashPlayerHTML(self, url):
+		code = '<object width="425" height="350"> \
+		       <param name="movie" value="' + url + '"></param> \
+		       <embed src="' + url + '" type="application/x-shockwave-flash" \
+		       width="425" height="350"></embed></object>'
+
+		return code
+	
+	def getAll(self):
+		client = gdata.youtube.service.YouTubeService()		
+		gdata.alt.appengine.run_on_appengine(client)		
+		
+		# retrieve the favorites
+		videos = client.GetYouTubeVideoFeed( self.getFeedUri( user=Config.getKey('youtube_user'), feed='favorites', count='11'))
+		for video in videos.entry:
+
+			if self.isDuplicate(video.id.text, 'youtube') == False:
+				e = Entry()
+				if video.title != None:
+					e.title = video.title.text.decode('UTF-8')
+				if video.content != None:
+					e.text = video.content.text.decode('UTF-8')
+				e.source = 'youtube'
+				e.external_id = video.id.text
+				e.created = parse( video.published.text )
+			
+				if video.media.keywords != None:
+					tag_lst = video.media.keywords.text.split(',')
+					e.tags = ' '.join(tag_lst)
+				e.put()
+			else:
+				print 'video is duplicate: ' + video.id.text
+		
+	def getLatest(self):
+		return self.getAll()
+			
+	def __processLinks(self, posts):		
+		pass
