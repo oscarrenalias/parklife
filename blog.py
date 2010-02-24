bea6716883c4621b1004774ccc8aed9367c0bee6
Oscar Renalias <oscar@renalias.net>
2010-02-25 00:49:46 +0200

    Added the javascript components: jquery and tiny_mce for visual edition of posts. Merged support for tinymce in the new blog post screen

--- blog.py
+++ blog.py
@@ -30,9 +30,9 @@ from django import newforms as forms
 # form object for the new blog entry
 #
 class BlogPostForm(forms.Form):
-	title = forms.CharField(label='Title for the blog post', widget=forms.widgets.TextInput(attrs={'size':60}))
-	text = forms.CharField(label='Text for the blog post',widget=forms.widgets.Textarea(attrs={'rows': 10, 'cols': 60 }))
-	tags = forms.CharField(required=False,label='Tags', widget=forms.widgets.TextInput(attrs={'size':60}))
+	title = forms.CharField(label='Title for the blog post', widget=forms.widgets.TextInput(attrs={'size':80}))
+	text = forms.CharField(label='Text for the blog post', widget=forms.widgets.Textarea(attrs={'rows': 14, 'cols': 80, 'class': 'mceEditor' }))
+	tags = forms.CharField(required=False,label='Tags', widget=forms.widgets.TextInput(attrs={'size':80}))
 	
 
 class BlogHandler(webapp.RequestHandler):	
