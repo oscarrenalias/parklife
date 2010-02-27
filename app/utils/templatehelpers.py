b7d00a0f51108323a938b8a3af629dccdf21b703
Oscar Renalias <oscar@renalias.net>
2010-02-27 21:34:45 +0200

    A couple of fixes for the Atom content generatin

--- app/utils/templatehelpers.py
+++ app/utils/templatehelpers.py
@@ -9,10 +9,20 @@ def permalink(value):
 		return value.permalink()
 	else:
 		raise Exception( 'The permalink filter only works on instances of the Entry class')
+		
+#
+# helper function to format a date to the RFC 3339 date used in Atom feeds
+#
+def atom_date(value):
+	if value.__class__.__name__ != "datetime":
+		raise Exception( 'This filter can only be used with datetime objects' )
+		
+	return(value.strftime("%Y-%m-%dT%H:%M:%SZ"))
 
 #
 # this is all necessary so that our custom filter is registered within django's filters
 #
 register = webapp.template.create_template_register()
 register.filter(permalink)
+register.filter(atom_date)
 webapp.template.register_template_library('app.utils.templatehelpers')
