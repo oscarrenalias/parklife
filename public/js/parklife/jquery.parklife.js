aea8d8a2e0e118ea8c290c59e15bb748fb67035b
unknown <oscar@.(none)>
2010-03-20 23:13:09 +0200

    jQuery extension that adds .put() and .delete() in addition to .post() and .get()

---
+++ public/js/parklife/jquery.parklife.js
@@ -0,0 +1,26 @@
+/* Extend jQuery with functions for PUT and DELETE requests. */
+
+function _ajax_request(url, data, callback, type, method) {
+	if ( jQuery.isFunction( data ) ) {
+		type = type || callback;
+		callback = data;
+		data = {};
+	}
+	
+    return jQuery.ajax({
+        type: method,
+        url: url,
+        data: data,
+        success: callback,
+        dataType: type
+    });
+}
+
+jQuery.extend({
+    put: function(url, data, callback, type) {
+        return _ajax_request(url, data, callback, type, 'PUT');
+    },
+    delete_: function(url, data, callback, type) {
+        return _ajax_request(url, data, callback, type, 'DELETE');
+    }
+});
