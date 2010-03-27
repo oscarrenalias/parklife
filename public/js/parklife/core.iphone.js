02261d07d362a2b6861101466fcca3bf97cab704
Oscar Renalias <oscar@renalias.net>
2010-03-27 18:24:12 +0200

    Added support for setting the location via iphone

--- public/js/parklife/core.iphone.js
+++ public/js/parklife/core.iphone.js
@@ -24,7 +24,26 @@ parklife.iphone.updateOrientation = function()
 	$("page_wrapper").css("class", contentType);
 }
 
+parklife.callbacks.updateLocation = function(loc)
+{
+	// update the latitude and longitude fields 
+	$('#id_lat').val(loc.coords.latitude);
+	$('#id_lng').val(loc.coords.longitude);
+
+	$('#location_update_button').toggle();
+	$('.spinner').remove();
+}
+
 $(document).ready(function(){
 	$("page_wrapper").css("visibility", "visible");	
 	window.scrollTo(0, 1); // pan to the bottom, hides the location bar  	
+	
+	// overwrite the behaviour of this button if we are in an iphone
+	$('#location_update_button').unbind("click");
+	$('#location_update_button').click(function() { 
+		$(this).toggle();
+		$(this).parent().append('<img src="/images/spinner.gif" class="spinner" />');
+		navigator.geolocation.getCurrentPosition(parklife.callbacks.updateLocation)
+	});		
 });
+
