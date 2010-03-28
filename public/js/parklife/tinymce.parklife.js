9b91cef1fd74a025cb71f15f437b82ecf2422fab
Oscar Renalias <oscar@renalias.net>
2010-03-28 23:53:04 +0300

    Minor correction to the regexp

--- public/js/parklife/tinymce.parklife.js
+++ public/js/parklife/tinymce.parklife.js
@@ -7,7 +7,7 @@ parklife.tinymce.CustomCleanup = function(type, content)
 	switch (type) {
 		// gets passed when user submits the form
 		case "get_from_editor":
-			content = content.replace(/<br \/>/gi, "\n"); // would be nice if this replace only replaces the brs in a pre block. bur for the moment it is ok.
+			content = content.replace(/<br\s*\/>/gi, "\n");
 			break;
 		// gets passed when new content is inserted into the editor
 		case "insert_to_editor":
