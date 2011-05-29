218bb9f3c03988ec23f289243119fedbbd1f54bd
Oscar Renalias <oscar@renalias.net>
2011-05-30 00:31:54 +0300

    First version of support for Ajax-based navigation wtihin links marked as rel="ajax"

---
+++ public/js/parklife/core.history.js
@@ -0,0 +1,132 @@
+(function(window,undefined){
+
+	// Prepare our Variables
+	var History = window.History;
+	var $ = window.jQuery;
+	var document = window.document;
+
+	// Check to see if History.js is enabled for our Browser
+	if ( !History.enabled ) {
+		return false;
+	}
+
+	// Wait for Document
+	$(function(){
+		// Prepare Variables
+		var contentSelector = '#content,article:first,.article:first,.post:first';
+		var $content = $(contentSelector).filter(':first');
+		var contentNode = $content.get(0);
+		var activeClass = 'active selected current youarehere';
+		var activeSelector = '.active,.selected,.current,.youarehere';
+
+		var $body = $(document.body);
+		var rootUrl = History.getRootUrl();
+		var scrollOptions = {
+				duration: 800,
+				easing:'swing'
+			};
+
+		// Ensure Content
+		if ( $content.length === 0 ) {
+			$content = $body;
+		}
+
+		// HTML Helper
+		var documentHtml = function(html) {
+			// Prepare
+			var result = String(html)
+				.replace(/<\!DOCTYPE[^>]*>/i, '')
+				.replace(/<(html|head|body|title|meta|script)/gi,'<div class="document-$1"')
+				.replace(/<\/(html|head|body|title|meta|script)/gi,'</div');
+
+			// Return
+			return result;
+		};
+
+		// Ajaxify Helper
+		$.fn.ajaxify = function(){
+			var $this = $(this);
+			$this.find('a[rel="ajax"]').click(function(event) {
+				var $this = $(this);
+				var url = $this.attr('href');
+				var title = $this.attr('title')||null;
+
+				// Continue as normal for cmd clicks etc
+				if ( event.which == 2 || event.metaKey ) { return true; }
+
+				// Ajaxify this link
+				History.pushState(null,title,url);
+				event.preventDefault();
+				return false;
+			});
+
+			return $this;
+		};
+
+		// Ajaxify our Internal Links
+		$body.ajaxify();
+
+		// Hook into State Changes
+		$(window).bind('statechange',function(){
+			// Prepare Variables
+			var State = History.getState()
+			var url = State.url;
+			var relativeUrl = url.replace(rootUrl,'');
+
+			// Set Loading
+			$body.addClass('loading');
+
+			// Start Fade Out
+			// Animating to opacity to 0 still keeps the element's height intact
+			// Which prevents that annoying pop bang issue when loading in new content
+			$content.animate({opacity:0},800);
+
+			// Ajax Request the Traditional Page
+			$.ajax({
+				url: url,
+				data: "b=1",
+				success: function(data, textStatus, jqXHR){
+					var $data = $(documentHtml(data));
+					var $dataContent = $data.filter(':first');					
+					var contentHtml; 
+					var $scripts;
+
+					// Fetch the scripts
+					$scripts = $dataContent.find('.document-script').detach();
+
+					// Fetch the content
+					contentHtml = $dataContent.html()||$data.html();
+					if ( !contentHtml ) {
+						document.location.href = url;
+						return false;
+					}
+
+					// Update the content
+					$content.stop(true,true);
+					$content.html(contentHtml).ajaxify().css('opacity',100).show();
+
+					// Add the scripts
+					$scripts.each(function(){
+						var scriptText = $(this).html(); 
+						var scriptNode = document.createElement('script');
+						scriptNode.appendChild(document.createTextNode(scriptText));
+						contentNode.appendChild(scriptNode);
+					});
+
+					// Complete the change
+					if ( $body.ScrollTo||false ) { $body.ScrollTo(scrollOptions); }
+					$body.removeClass('loading');
+
+					// Inform Google Analytics of the change
+					if ( typeof window.pageTracker !== 'undefined' ) {
+						window.pageTracker._trackPageview(relativeUrl);
+					}
+				},
+				error: function(jqXHR, textStatus, errorThrown){
+					document.location.href = url;
+					return false;
+				}
+			});
+		});
+	});
+})(window); 
