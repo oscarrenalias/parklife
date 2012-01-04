ef7a06fe21e77f2da8bfd75687adbafcf5de99a5
Oscar Renalias <oscar@renalias.net>
2012-01-04 21:36:26 +0200

    Added some tests (not working yet)

--- app/tests/test_viewhelpers.py
+++ app/tests/test_viewhelpers.py
@@ -15,5 +15,4 @@ class TestViewHelpers(unittest.TestCase):
 	def testiPadUA(self):
 		# should not match either, this is the iPad user agent string
 		self.assertEqual(False, 
-			ViewHelpers.is_iphone('Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10'))		
-				
+			ViewHelpers.is_iphone('Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10'))
